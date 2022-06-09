5920fc169dd08549c6be8d24380f066fc7c9a8fb
Leo Ngari <ngareoel@gmail.com>
2022-06-10 00:38:19 +0300

    Classifier

--- loader.py
+++ loader.py
@@ -1,10 +1,15 @@
 import os
 import tarfile
+import numpy as np
+from sklearn.datasets import fetch_openml
 from six.moves import urllib
 
 DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
 HOUSING_PATH = os.path.join("datasets", "housing")
 HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+MNIST_PATH = os.path.join("datasets", "mnist")
+
 
 def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     if not os.path.isdir(housing_path):
@@ -15,6 +20,13 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz = tarfile.open(tgz_path)
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
+
+
+def get_mnist_data(file_location=MNIST_PATH):
+    mnist = fetch_openml("mnist_784", version=1)
+    digits, labels = np.array(mnist.get("data")), np.array(mnist.get("target"))
+
+    return (digits, labels)
 
 
 if __name__ == "__main__":
--- operate.py
+++ operate.py
@@ -11,16 +11,5 @@ def load_housing_data(filename, housing_path=HOUSING_PATH):
 
 if __name__ == "__main__":
     housing_data = load_housing_data(filename="housing.csv")
-    housing_data.plot(
-        kind="scatter",
-        x="longitude",
-        y="latitude",
-        alpha=0.4,
-        s=housing_data["population"]/100,
-        label="population",
-        figsize=(10, 7),
-        c="median_house_value",
-        cmap=plt.get_cmap("jet"),
-        colorbar=True
-        )
-    plt.show()
+    u = housing_data.describe()
+    print(u)
